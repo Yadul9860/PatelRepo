22b7f2f11b16178d2c32103d1a8c9abac13e974d
Yadul <yadulpatel16@gmail.com>
2022-11-20 13:22:46 +0530

    Add project files.

---
+++ B18Csharp demo/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B18Csharp_demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  palindrome number
+            //Console.WriteLine("please enter number");
+            //int num = Convert.ToInt32(Console.ReadLine());
+            //int rem = 0;
+            //int result = 0;
+            //while (num != 0)
+            //{
+            //    rem = num % 10;
+            //    result = result * 10 + rem;
+            //    num = num / 10;
+            //}
+            //Console.WriteLine(result);
+            //Console.ReadLine();
+
+            // Reverse duplicate
+
+            string name = "yadulpatel";
+            string name1= string.Empty;
+            for(int i = 0; i< name.Length; i++)
+            {
+                if (!name1.Contains(name[i]))
+                {
+                    name1 += name[i];
+                }
+            }
+            Console.WriteLine(name1);
+            Console.ReadLine();
+
+        }
+    }
+}
